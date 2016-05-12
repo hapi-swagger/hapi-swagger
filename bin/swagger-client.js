74cd01434678470f04e6f098a8d48133886fc4cd
Rui Marques <ruionwriting@gmail.com>
2016-05-12 11:20:43 +0100

    Enhance visual grouping capabilities when supporting different API versions
    Having multi-version API I want to have a good and simple solution to
    allow multiple-version documentation but I don't want my groups being
    affected by the basePath neither the version bit of it.
    
    API consumes are also required to supply Accept header like
    'application/vnd.mysuperapi.v2+json'. Internally end-points evolve over
    time and routing is like:
    
    GET /api/v1/customers/ - this version will be supported for months.
    GET /api/v1/customers/{guid} - this version will be supported for
    months.
    GET /api/v2/customers/{guid}
    ...
    
    Each route can this way have it's own validation and documentation. We
    don't want to clutter the documentation with the version fragments. On
    this example when viewing documentation for v2 the user will see a list
    operation that is the same since v1 and the new detail operation from
    v2.
    
    When a request arrives to the API based on the Accept header version
    information the proper route path is set. No ugly v<int> on my URIs.

--- bin/swagger-client.js
+++ bin/swagger-client.js
@@ -19,6 +19,7 @@ server.connection({
 
 let swaggerOptions = {
     basePath: '/v1/',
+    suppressVersionFromBasePath: false,
     pathPrefixSize: 2,
     info: {
         'title': 'Test API Documentation',
