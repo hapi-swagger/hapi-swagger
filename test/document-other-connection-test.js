02d7fafc31ca1c238240fb8514d463e81ab11042
Glenn Jones <glennjonesnet@gmail.com>
2016-08-04 11:11:39 +0100

    Correct error in test call

--- test/document-other-connection-test.js
+++ test/document-other-connection-test.js
@@ -85,7 +85,7 @@ lab.experiment('document another connection', () => {
             //console.log(JSON.stringify(response.result.paths['/plugin1']))
             expect(response.statusCode).to.equal(200);
             expect(response.result.host).to.equal('localhost:3000');
-            expect(response.result.paths['/plugin1']).to.deep.equal({
+            expect(response.result.paths['/plugin1']).to.equal({
 
                 'get': {
                     'tags': [
